8c1b2065adfe952a4fad333f588a9dd365e02efa
grahamfw83 <grahamfw83@BHS-LWG105-20.Boerne-ISD.net>
2016-10-27 16:00:03 -0500

    end of class 10/27

--- Pre ap comp sci/src/chapter3/BooleanTest.java
+++ Pre ap comp sci/src/chapter3/BooleanTest.java
@@ -1,5 +1,20 @@
 package chapter3;
 
 public class BooleanTest {
-
+	public static void main(String[] args) {
+	double number1, number2, number3, sign1, sign2, sign3;
+	number1 = Math.random()*100-50;
+	number2 = Math.random()*100-50;
+	number3 = Math.random()*100-50;
+	
+	boolean eval1 = number1<0;
+	if (eval1)
+		System.out.println("YO BOI NUMBER 1 IS NEGATIVE HOLLA");
+	eval1 = number2<0;
+    if (eval1)
+		System.out.println("YO BOI NUMBER 2 IS NEGATIVE HOLLA"); 
+    eval1 = number3<0;
+    if (eval1)
+		System.out.println("YO BOI NUMBER 3 IS NEGATIVE HOLLA");
+	}
 }
---
+++ Pre ap comp sci/src/chapter3/Payroll.java
@@ -0,0 +1,32 @@
+package chapter3;
+import java.util.Scanner;
+public class Payroll {
+
+	public static void main(String[] args) {
+		double hoursStandard, hoursOvertime, payStandard, payOvertime, payTotal, hoursTotal;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter the amount of hours you have worked please right now in the last week more specifically based upon a standard work rate of 40 hours");
+		hoursStandard = keyboard.nextDouble();
+		
+		if(hoursStandard>=40){
+			hoursOvertime = hoursStandard-40;
+			hoursStandard = 40;
+		}else{
+			hoursOvertime = 0;
+		}
+		System.out.println("enter pay rate");
+		payStandard = keyboard.nextDouble();
+		keyboard.close();
+		
+		payOvertime = hoursOvertime*(payStandard*1.5);
+		payStandard = hoursStandard*payStandard;
+		hoursTotal = hoursStandard+hoursOvertime;
+		payTotal = payStandard+payOvertime;
+		
+		System.out.println("Hours worked: " + hoursTotal + "\nOvertime hours: " + hoursOvertime + "\nOvertime pay: " + payOvertime + "\nTotal pay " + payTotal);
+		
+
+	}
+
+}
