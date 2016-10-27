c48c15d02357d3027cb9e7c943f33907a4ff715f
grahamfw83 <grahamfw83@BHS-LWG105-20.Boerne-ISD.net>
2016-10-27 14:51:52 -0500

    day

---
+++ Pre ap comp sci/src/chapter3/BasicIfStatement.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class BasicIfStatement {
+
+	public static void main(String[] args) {
+		double bonus;
+		int sales = 500000;
+		if (sales>5000);
+		    bonus = 500.00;
+
+	}
+
+}
