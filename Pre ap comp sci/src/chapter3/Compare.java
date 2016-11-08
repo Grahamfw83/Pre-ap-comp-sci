159941501d5b3342298d0f6ab6de7b2c9625ce9d
grahamfw83 <grahamfw83@BHS-LWG105-20.Boerne-ISD.net>
2016-11-08 16:00:31 -0600

    end of class

---
+++ Pre ap comp sci/src/chapter3/Compare.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+public class Compare {
+
+	public static void main(String[] args) {
+		String name1 = "paul";
+		String name2 = "gabe";
+		
+		
+		
+
+	}
+
+}
--- Pre ap comp sci/src/grahamwhidbee/labs/DateTester.java
+++ Pre ap comp sci/src/grahamwhidbee/labs/DateTester.java
@@ -1,9 +1,16 @@
 package grahamwhidbee.labs;
-
+import java.util.Scanner;
 public class DateTester {
 
 	public static void main(String[] args) {
+		int month, day, year;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter a day, month and year");
+		day = keyboard.nextInt();
+		month = keyboard.nextInt();
+		year = keyboard.nextInt();
 		
+		if ()
 
 	}
 
