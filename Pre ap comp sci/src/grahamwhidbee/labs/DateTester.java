35d9c11bebdc36a37284ae9290a90466254faa4c
grahamfw83 <grahamfw83@BHS-LWG105-20.Boerne-ISD.net>
2016-11-08 16:00:40 -0600

    end of class2

--- Pre ap comp sci/src/grahamwhidbee/labs/DateTester.java
+++ Pre ap comp sci/src/grahamwhidbee/labs/DateTester.java
@@ -9,8 +9,34 @@ public class DateTester {
 		day = keyboard.nextInt();
 		month = keyboard.nextInt();
 		year = keyboard.nextInt();
-		
-		if ()
+		keyboard.close();
+		switch(month) {
+			case 1 :
+				System.out.println("It be winter time in the city");
+			case 2 :
+				System.out.println("It be winter time in the city");
+			case 3 :
+				
+			case 4 :
+				
+			case 5 :
+				
+			case 6 :
+				
+			case 7 :
+				
+			case 8 :
+				
+			case 9 :
+				
+			case 10 :
+				
+			case 11 :
+				
+			case 12 :
+				System.out.println("It be winter time in the city");
+				System.out.println("MERRY FRICKIN CHRISTMAS");
+		}
 
 	}
 
