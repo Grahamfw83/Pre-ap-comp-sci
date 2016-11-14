8f9582d85688c47b30515beebaf29075a493092e
grahamfw83 <grahamfw83@BHS-LWG105-20.Boerne-ISD.net>
2016-11-14 16:01:36 -0600

    salty

--- Pre ap comp sci/src/ch4/WhileLoop.java
+++ Pre ap comp sci/src/ch4/WhileLoop.java
@@ -7,16 +7,7 @@ public class WhileLoop {
 		my = 0;
 		butt = 20;
 		
-		while (my<butt){
-			my++;
-				if (my==2||my==4||my==6||my==8||my==10||my==12||my==14||my==16||my==18||my==20){
-					System.out.println(my);
-				}
-			
-
-			
-			
-			}
+		
 		
 	}
 
