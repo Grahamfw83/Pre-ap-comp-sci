92a937e112f580f733106170438c14831fe3cbd3
grahamfw83 <grahamfw83@BoerneISD.net>
2016-12-09 15:59:18 -0600

    end of class friday

--- Pre ap comp sci/src/ch4/MileTimeCalculator.java
+++ Pre ap comp sci/src/ch4/MileTimeCalculator.java
@@ -3,18 +3,26 @@ package ch4;
 public class MileTimeCalculator {
 
 	public static void main(String[] args) {
-		double amountOfMiles, avgMileTimeDecimal, tempSeconds;
+		double avgMileTimeDecimal, minutesInt, secondsInt;
+		int indexOfLocation;
 		String minutes, seconds;
-		String avgMileTime = "7:30";
-		amountOfMiles = 3.1;
 		
-		minutes = avgMileTime.substring(0);
-		tempSeconds = avgMileTime.indexOf(":");
-		seconds = avgMileTime.substring((int) tempSeconds+1);
-		System.out.println("do you even understand that you are the ");
+		final String avgMileTime = "7:38";
+		double amountOfMiles = 26.2;
 		
 		
+		indexOfLocation = avgMileTime.indexOf(":");
+		minutes = avgMileTime.substring(0, indexOfLocation);
+		seconds = avgMileTime.substring(indexOfLocation+1);
 		
+		minutesInt = Integer.parseInt(minutes);
+		secondsInt = Double.parseDouble(seconds);
+		
+		minutesInt *= amountOfMiles;
+		secondsInt = secondsInt*amountOfMiles;
+		System.out.println(seconds);
+		avgMileTimeDecimal = (secondsInt/100)+minutesInt;
+		System.out.println(avgMileTimeDecimal);
 
 	}
 
