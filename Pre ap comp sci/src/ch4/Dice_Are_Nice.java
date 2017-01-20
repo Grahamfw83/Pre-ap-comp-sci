ad043c76e52cf5beddd4a7694a82c78030d496e9
Gam <Gam@DESKTOP-L1DFAJ1>
2017-01-20 15:59:08 -0600

    End of class 1/20/2017

--- Pre ap comp sci/src/ch4/Dice_Are_Nice.java
+++ Pre ap comp sci/src/ch4/Dice_Are_Nice.java
@@ -4,11 +4,24 @@ public class Dice_Are_Nice {
 
 	public static void main(String[] args) {
 		double dice1, dice2;
+		int dice1Int, dice2Int;
 		boolean win = false;
 		while(!win){
-			dice1 = Math.random()*6+1;
-			switch(){
-			
+			dice1 = (int)(Math.random()*6)+1;
+			dice2 = (int)(Math.random()*6)+1;
+			dice1Int = (int)dice1;
+			dice2Int = (int)dice2;
+			if (dice1Int+dice2Int==2||dice1Int+dice2Int==3||dice1Int+dice2Int==11||dice1Int+dice2Int==12) {
+				System.out.println("dice 1 = " +dice1Int + "\ndice 2 equals " + dice2Int);
+				System.out.print("The total is ");
+				System.out.println(dice1Int+dice2Int);
+				System.out.println("You win!");
+				win = true;
+			}else if (dice1+dice2==7){
+				System.out.println("You Lose! HA");
+				win = true;
+			}else {
+				System.out.println("You didnt roll a winning number or a losing number, game will run again");
 			}
 		}
 		
